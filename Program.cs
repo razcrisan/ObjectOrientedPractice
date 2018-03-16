b683da8e48d58022d4b7496b0ea45d0fe6dc370f
Razvan Crisan <rcrisan13@gmail.com>
2018-03-16 12:10:15 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOPPractice
+{
+    public class Warrior
+    {
+        private int height;
+        private int weight;
+        private string power;
+        
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public string Power { get; set; }
+    }
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Warrior sangwynn = new Warrior();
+            sangwynn.Height = 190;
+            sangwynn.Weight = 80;
+            sangwynn.Power = "One For All";
+
+            Warrior theBadGuy = new Warrior();
+            theBadGuy.Height = 150;
+            theBadGuy.Weight = 40;
+            theBadGuy.Power = "All For One";
+            System.Console.WriteLine(theBadGuy.Power);
+            System.Console.WriteLine(sangwynn.Power);
+        }
+    }
+}
